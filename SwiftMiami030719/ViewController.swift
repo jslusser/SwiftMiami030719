5bc09d7c3987cd258bace4ff06a42fdfa761ba36
Jim Slusser <jslusser@gmail.com>
2019-03-04 17:23:09 -0500

    Initial commit

--- SwiftMiami030719/ViewController.swift
+++ SwiftMiami030719/ViewController.swift
@@ -6,15 +6,42 @@
 //  Copyright © 2019 James Slusser. All rights reserved.
 //
 
+// UIProgressView
+
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    let progress = Progress(totalUnitCount: 10)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        progressView.transform = progressView.transform.scaledBy(x: 1, y: 5)
     }
-
-
+    
+    @IBAction func didTapStartProgress(_ sender: Any) {
+        
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
+            
+            guard self.progress.isFinished == false else {
+                timer.invalidate()
+                print("finished")
+                
+                return
+            }
+            
+            self.progress.completedUnitCount += 1
+            
+            let progressFloat = Float(self.progress.fractionCompleted)
+            self.progressView.setProgress(progressFloat, animated: true)
+            
+            
+        }
+        
+        
+    }
+    
 }
-
